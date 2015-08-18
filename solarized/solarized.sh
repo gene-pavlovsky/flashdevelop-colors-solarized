c447936b27df9fe9e31da289a9c8f218d1551502
Gene Pavlovsky <gene.pavlovsky@gmail.com>
2015-08-19 01:24:04 +0700

    solarized.sh: Package generated themes into fdz, then clean up the work files

--- solarized/solarized.sh
+++ solarized/solarized.sh
@@ -1,8 +1,12 @@
 #!/bin/sh
-
 SUB="./solarized_sub.sh"
 DARK_OPTIONS=""
 LIGHT_OPTIONS="-l -H"
+
+ZIP="7z a -tzip"
+BASE_DIR="\$(BaseDir)"
+SYNTAX_DIR="$BASE_DIR/Settings/Languages"
+THEME_DIR="$BASE_DIR/Settings/Themes"
 
 mkdir dark light 2>/dev/null
 
@@ -19,13 +23,38 @@ echo "Themes:"
 echo "  SolarizedDark.fdi"
 sed -e 's,{solarized},SolarizedDark,' \
 		-e 's,{image_set},Darker,' \
-		templates/Solarized.fdi | "$SUB" $DARK_OPTIONS -n - >dark/SolarizedDark.fdi
-rm -f dark/CURRENT
-ln dark/SolarizedDark.fdi dark/CURRENT
+		templates/Solarized.fdi | "$SUB" $DARK_OPTIONS -n - >dist/SolarizedDark.fdi
 
 echo "  SolarizedLight.fdi"
 sed -e 's,{solarized},SolarizedLight,' \
 		-e 's,{image_set},Default,' \
-		templates/Solarized.fdi | "$SUB" $LIGHT_OPTIONS -n - >light/SolarizedLight.fdi
-rm -f light/CURRENT
-ln light/SolarizedLight.fdi light/CURRENT
+		templates/Solarized.fdi | "$SUB" $LIGHT_OPTIONS -n - >dist/SolarizedLight.fdi
+
+echo
+echo "Packaging themes:"
+
+package_theme() {
+	echo "$1:"
+	rm -rf "$BASE_DIR"
+	mkdir -p "$SYNTAX_DIR"
+	cp ../${1,,}/*.xml* "$SYNTAX_DIR"
+	rm -f SyntaxThemes/Solarized$1.fdz
+	$ZIP SyntaxThemes/Solarized$1.fdz "$BASE_DIR"
+	echo
+	mkdir -p "$THEME_DIR"
+	cp Solarized$1.fdi "$THEME_DIR"
+	cp Solarized$1.fdi "$THEME_DIR/CURRENT"
+	rm -f FullThemes/Solarized$1.fdz
+	$ZIP FullThemes/Solarized$1.fdz "$BASE_DIR"
+	rm -rf "$BASE_DIR"
+	echo
+}
+
+cd dist
+package_theme Dark
+package_theme Light
+cd ..
+
+echo -n "Cleaning up... "
+rm -rf dark light
+echo "done"
